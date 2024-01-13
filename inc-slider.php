0ca5d32563a3bffe2e1c7d465a06455b15bc48ee
Lukasz (akrea.pl) <info@akrea.pl>
2024-01-13 11:12:55 +0100

    slider

--- inc-slider.php
+++ inc-slider.php
@@ -1,24 +1,47 @@
-<div id="carouselExampleDark" class="carousel carousel-dark slide">
-    <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
-    <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="10000">
-            <img style="height:800px; width: auto;" src="https://images.unsplash.com/photo-1450101499163-c8848c66ca85?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h3>First slide label</h3>
-                <p>Some representative placeholder content for the first slide.</p>
+<div class="container">
+    <div id="carouselExampleDark" class="carousel carousel-dark slide">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="10000">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/slider1.jpg" class="" alt="...">
+                <div class="carousel-caption d-none d-md-block" style="position: absolute; top:100px; right:40px;">
+                    <h3 clas>First slide label</h3>
+                    <p>Some representative placeholder content for the first slide.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/slider2.jpg" class="" alt="...">
+                <div class="carousel-caption d-none d-md-block" style="position: absolute; top:100px; right:40px;">
+                    <h3>First slide label</h3>
+                    <p>Some representative placeholder content for the first slide.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/slider3.jpg" class="" alt="...">
+                <div class="carousel-caption d-none d-md-block" style="position: absolute; top:100px; right:40px;">
+                    <h3>First slide label</h3>
+                    <p>Some representative placeholder content for the first slide.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/slider4.jpg" class="" alt="...">
+                <div class="carousel-caption d-none d-md-block" style="position: absolute; top:100px; right:40px;">
+                    <h3>First slide label</h3>
+                    <p>Some representative placeholder content for the first slide.</p>
+                </div>
             </div>
         </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Poprzedni</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Następny</span>
+        </button>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
 </div>
