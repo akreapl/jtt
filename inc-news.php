6aab4f42d16818938596f04739f7fc1df5357fc9
Lukasz (akrea.pl) <info@akrea.pl>
2024-01-14 16:02:59 +0100

    news style

--- inc-news.php
+++ inc-news.php
@@ -7,7 +7,7 @@
             </div>
             <?php shownews(2, 4); ?>
             <div class="col-md-4 col-sm-12 text-center my-5">
-                <a href="https://jtt.akrea.pl/category/aktualnosci/" class="w-100 btn btn-outline-primary my-3">WIĘCEJ ARTYKUŁÓW</a>
+                <a href="https://jtt.akrea.pl/category/aktualnosci/" class="w-100 btn btn-outline-primary my-3">WIĘCEJ ARTYKUŁÓW <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg></a>
             </div>
         </div>
     </div>
