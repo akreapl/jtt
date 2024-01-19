bdf0675aa4eda5da5d01034cd914765ade7cbab7
Lukasz (akrea.pl) <info@akrea.pl>
2024-01-19 19:25:58 +0100

    hero on mobile homepage

--- inc-slider.php
+++ inc-slider.php
@@ -2,36 +2,36 @@
     <div class="carousel-inner">
         <div class="carousel-item bg-slider active position-relative overflow-hidden" data-bs-interval="10000">
             <img src="<?php echo get_template_directory_uri(); ?>/img/v3/1.svg" class="" alt="...">
-            <div class="carousel-captionaaa d-none d-md-block position-absolute" style="width:35%; top:80px; right:15%;">
+            <div class="d-none d-md-block position-absolute" style="width:35%; top:80px; right:15%;">
                 <div>
-                    <h1 class="slider-header py-4">1 Z nami jesteś na wyższej pozycji</h1>
+                    <h1 class="slider-header py-4">Z nami jesteś na wyższej pozycji</h1>
                     <p class="fs-4">Pracujemy ciężko, aby rozwiązywać spory wcześnie i polubownie, jeżeli to tylko możliwe, często załatwiając sprawy konfliktowe jeszcze przed wniesieniem pozwu.</p>
                 </div>
             </div>
         </div>
         <div class="carousel-item bg-slider position-relative overflow-hidden" data-bs-interval="10000">
             <img src="<?php echo get_template_directory_uri(); ?>/img/v3/2.svg" class="" alt="...">
-            <div class="carousel-captionaaa d-none d-md-block position-absolute" style="width:35%; top:80px; right:15%;">
+            <div class="d-none d-md-block position-absolute" style="width:35%; top:80px; right:15%;">
                 <div>
-                    <h1 class="slider-header py-4">1 Nie zatrudniaj nas, jeśli to proste</h1>
+                    <h1 class="slider-header py-4">Nie zatrudniaj nas, jeśli to proste</h1>
                     <p class="fs-4">Jeśli masz trudny, skomplikowany lub nietypowy spór biznesowy albo prywatny, jesteśmy gotowi podjąć każde wyzwanie.</p>
                 </div>
             </div>
         </div>
         <div class="carousel-item bg-slider position-relative overflow-hidden" data-bs-interval="10000">
             <img src="<?php echo get_template_directory_uri(); ?>/img/v3/3.svg" class="" alt="...">
-            <div class="carousel-captionaaa d-none d-md-block position-absolute" style="width:35%; top:80px; right:15%;">
+            <div class="d-none d-md-block position-absolute" style="width:35%; top:80px; right:15%;">
                 <div>
-                    <h1 class="slider-header py-4">1 Nie podążamy schematem, piszemy historię</h1>
+                    <h1 class="slider-header py-4">Nie podążamy schematem, piszemy historię</h1>
                     <p class="fs-4">Analizujemy wszystkie aspekty Twojej działalności i/lub problemu prawnego z kreatywną perspektywą, wykorzystując nie tylko suche fakty lub prawo, aby znaleźć unikalne podejście do rozwiązania Twoich problemów prawnych. Zawsze toczymy właściwą walkę, we właściwy sposób.</p>
                 </div>
             </div>
         </div>
         <div class="carousel-item bg-slider position-relative overflow-hidden" data-bs-interval="10000">
             <img src="<?php echo get_template_directory_uri(); ?>/img/v3/4.svg" class="" alt="...">
-            <div class="carousel-captionaaa d-none d-md-block position-absolute" style="width:35%; top:80px; right:15%;">
+            <div class="d-none d-md-block position-absolute" style="width:35%; top:80px; right:15%;">
                 <div>
-                    <h1 class="slider-header py-4">1 Zaczynamy od Twojego końcowego rezultatu</h1>
+                    <h1 class="slider-header py-4">Zaczynamy od Twojego końcowego rezultatu</h1>
                     <p class="fs-4">Większość kancelarii prawnych nie rozumie końcowego rezultatu, który chcesz osiągnąć, skupiając się jedynie na rozpoczęciu działań. Naszym pierwszym krokiem jest współpraca z Tobą w celu wyjaśnienia Twoich celów oraz sytuacji prawnej i/lub biznesowej, w jakiej się znajdujesz.</p>
                 </div>
             </div>
@@ -47,16 +47,9 @@
     </button>
 </div>
 
-<div id="carouselExampleDark" class="carousel carousel-dark slide carousel-fade d-block d-md-none" style="margin-top:70px;">
-    <div class="carousel-inner">
-        <div class="carousel-item bg-slider active position-relative overflow-hidden" data-bs-interval="10000">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/1.svg" class="" alt="...">
-            <div class="carousel-captionaaa d-none d-md-block position-absolute" style="width:35%; top:80px; right:15%;">
-                <div>
-                    <h1 class="slider-header py-4">Z nami jesteś na wyższej pozycji</h1>
-                    <p class="fs-4">Pracujemy ciężko, aby rozwiązywać spory wcześnie i polubownie, jeżeli to tylko możliwe, często załatwiając sprawy konfliktowe jeszcze przed wniesieniem pozwu.</p>
-                </div>
-            </div>
-        </div>
+<div class="d-block d-md-none" style="margin-top:70px;">
+    <div class="d-flex align-content-center align-items-center justify-content-end" style="height:350px; background-image: url(<?php echo get_template_directory_uri(); ?>/img/v3/slider-mobile3.svg); background-size:cover;">
+        <!-- <img src="<?php echo get_template_directory_uri(); ?>/img/v3/slider-mobile2.svg" class="" alt="..."> -->
+        <h1 class="slider-header ms-5 pb-5 mb-5">Nie podążamy schematem, <span class="text-primary fw-bolder fst-italic">piszemy historię</span></h1>
     </div>
 </div>
